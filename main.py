2f7640a40f2ecc717cc88c4691620343830837a5
itsshivangi <Shivangishivangi@my.unt.edu>
2025-11-01 21:03:17 -0500

    completed main file with the snake game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle,Screen
+from turtle import Screen
 from snake import Snake 
+from food import Food
+from scoreboard import Scoreboard
 import random
 import time   #1 Import the time module to control the speed of the snake
 
@@ -10,8 +12,11 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)  # Turn off automatic screen updates for smoother animation
 
-
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -24,7 +29,25 @@ while game_is_on:
     time.sleep(0.1)  # Pause for a short duration to control the speed of the snake (time module)
 
     snake.move()  # Move the snake forward
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if (snake.head.xcor() > 290 or snake.head.xcor() < -290 or
+            snake.head.ycor() > 290 or snake.head.ycor() < -290):
+        game_is_on = False
+        scoreboard.goto(0, 0)
+        scoreboard.write("GAME OVER", align="center", font=("Courier", 30, "normal"))
      
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
